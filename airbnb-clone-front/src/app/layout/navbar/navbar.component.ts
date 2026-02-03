f5b51d87b104c3ab27db7990dc186f7969e597d1
pagarciaSim <pablo.garciasimavilla@gmail.com>
2026-02-03 07:07:57 +0100

    feat(auth) integrating auth & primeng menuitems in navbar

--- airbnb-clone-front/src/app/layout/navbar/navbar.component.ts
+++ airbnb-clone-front/src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonModule } from 'primeng/button';
 import { DialogService } from "primeng/dynamicdialog";
@@ -8,6 +8,8 @@ import { AvatarComponent } from './avatar/avatar.component';
 import { CategoryComponent } from './category/category.component';
 import { MenuItem } from 'primeng/api';
 import { ToastService } from '../toast.service';
+import { AuthService } from '../../core/auth/auth.service';
+import { User } from '../../core/model/user.model';
 
 @Component({
   selector: 'app-navbar',
@@ -26,38 +28,76 @@ import { ToastService } from '../toast.service';
 })
 export class NavbarComponent implements OnInit {
 
+  toastService = inject(ToastService);
+  authService = inject(AuthService);
   location = "Anywhere";
   guests = "Add guests";
   dates = "Any week";
   currentMenuItems: MenuItem[] | undefined = [];
+  connectedUser: User = {email: this.authService.notConnected};
 
-  toastService: ToastService = inject(ToastService);
+  constructor() {
+    effect(() => {
+      if (this.authService.fetchUser().status === "OK") {
+        this.connectedUser = this.authService.fetchUser().value!;
+        this.currentMenuItems = this.fetchMenu();
+      }
+    });
+  }
 
   ngOnInit(): void {
-    this.fetchMenu();
-    this.toastService.send({severity:'info', summary: 'Welcome to your Airbnb App'});
+    this.authService.fetch(false);
   }
 
   private fetchMenu(): MenuItem[] {
-    return [
-      {
-        label: "Sign up",
-        styleClass: "font-bold",
-      },
-      {
-        label: "Log in",
-      }
-    ]
+    if (this.authService.isAuthenticated()) {
+      return [
+        {
+          label: "My properties",
+          routerLink: "landlord/properties",
+          visible: this.hasToBeLandlord(),
+        },
+        {
+          label: "My booking",
+          routerLink: "booking",
+        },
+        {
+          label: "My reservation",
+          routerLink: "landlord/reservation",
+          visible: this.hasToBeLandlord(),
+        },
+        {
+          label: "Log out",
+          command: this.logout
+        },
+      ]
+    } else {
+      return [
+        {
+          label: "Sign up",
+          styleClass: "font-bold",
+          command: this.login
+        },
+        {
+          label: "Log in",
+          command: this.login
+        }
+      ];
+    }
+  }
+
+  hasToBeLandlord(): boolean {
+    return this.authService.hasAnyAuthority("ROLE_LANDLORD");
   }
 
   openNewSearch() {
-throw new Error('Method not implemented.');
-}
-openNewListing() {
-throw new Error('Method not implemented.');
-}
+    throw new Error('Method not implemented.');
+  }
+  openNewListing() {
+    throw new Error('Method not implemented.');
+  }
 
-  //login = () => this.authService.login();
+  login = () => this.authService.login();
 
-  //logout = () => this.authService.logout();
+  logout = () => this.authService.logout();
 }
