b40d4238c4ac6723bc256abff5560af26b0b9f80
pagarciaSim <pablo.garciasimavilla@gmail.com>
2026-02-03 06:30:11 +0100

    feat(auth) auth expired interceptor

---
+++ airbnb-clone-front/src/app/core/auth/auth-expired.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpErrorResponse } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { tap } from "rxjs";
+import { AuthService } from "./auth.service";
+
+export const authExpired: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) => {
+  const authService = inject(AuthService);
+  return next(req).pipe(
+    tap({
+        error: (err: HttpErrorResponse) => {
+        // Expired authentication token
+        if(err.status === 401 && err.url && !err.url.includes("api/auth") && authService.isAuthenticated()) {
+          authService.login();
+        }
+      }
+    })
+  )
+}
