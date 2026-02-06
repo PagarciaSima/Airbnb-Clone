3d05edd861b2d01633024d5cf09a7dc77c5e9dbb
pagarciaSim <pablo.garciasimavilla@gmail.com>
2026-02-06 17:31:14 +0100

    Feat LandLord & Picture serce & Auth0Service

---
+++ airbn-clone-back/src/main/java/com/airbnb/airbn_clone_back/listing/LandlordService.java
@@ -0,0 +1,87 @@
+package com.airbnb.airbn_clone_back.listing;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.airbnb.airbn_clone_back.listing.application.dto.CreatedListingDTO;
+import com.airbnb.airbn_clone_back.listing.application.dto.DisplayCardListingDTO;
+import com.airbnb.airbn_clone_back.listing.application.dto.ListingCreateBookingDTO;
+import com.airbnb.airbn_clone_back.listing.application.dto.SaveListingDTO;
+import com.airbnb.airbn_clone_back.listing.domain.Listing;
+import com.airbnb.airbn_clone_back.listing.mapper.ListingMapper;
+import com.airbnb.airbn_clone_back.listing.repository.ListingRepository;
+import com.airbnb.airbn_clone_back.sharedkernel.service.State;
+import com.airbnb.airbn_clone_back.user.application.Auth0Service;
+import com.airbnb.airbn_clone_back.user.application.UserService;
+import com.airbnb.airbn_clone_back.user.application.dto.ReadUserDTO;
+@Service
+public class LandlordService {
+
+    private final ListingRepository listingRepository;
+
+    private final ListingMapper listingMapper;
+    private final UserService userService;
+    private final Auth0Service auth0Service;
+    private final PictureService pictureService;
+
+    public LandlordService(ListingRepository listingRepository, ListingMapper listingMapper, UserService userService, Auth0Service auth0Service, PictureService pictureService) {
+        this.listingRepository = listingRepository;
+        this.listingMapper = listingMapper;
+        this.userService = userService;
+        this.auth0Service = auth0Service;
+        this.pictureService = pictureService;
+    }
+
+    public CreatedListingDTO create(SaveListingDTO saveListingDTO) {
+        Listing newListing = listingMapper.saveListingDTOToListing(saveListingDTO);
+
+        ReadUserDTO userConnected = userService.getAuthenticatedUserFromSecurityContext();
+        newListing.setLandlordPublicId(userConnected.publicId());
+
+        Listing savedListing = listingRepository.saveAndFlush(newListing);
+
+        pictureService.saveAll(saveListingDTO.getPictures(), savedListing);
+
+        auth0Service.addLandlordRoleToUser(userConnected);
+
+        return listingMapper.listingToCreatedListingDTO(savedListing);
+    }
+
+    @Transactional(readOnly = true)
+    public List<DisplayCardListingDTO> getAllProperties(ReadUserDTO landlord) {
+        List<Listing> properties = listingRepository.findAllByLandlordPublicIdFetchCoverPicture(landlord.publicId());
+        return listingMapper.listingToDisplayCardListingDTOs(properties);
+    }
+
+    @Transactional
+    public State<UUID, String> delete(UUID publicId, ReadUserDTO landlord) {
+        long deletedSuccessfuly = listingRepository.deleteByPublicIdAndLandlordPublicId(publicId, landlord.publicId());
+        if (deletedSuccessfuly > 0) {
+            return State.<UUID, String>builder().forSuccess(publicId);
+        } else {
+            return State.<UUID, String>builder().forUnauthorized("User not authorized to delete this listing");
+        }
+    }
+
+    public Optional<ListingCreateBookingDTO> getByListingPublicId(UUID publicId) {
+        return listingRepository.findByPublicId(publicId).map(listingMapper::mapListingToListingCreateBookingDTO);
+    }
+
+    public List<DisplayCardListingDTO> getCardDisplayByListingPublicId(List<UUID> allListingPublicIDs) {
+        return listingRepository.findAllByPublicIdIn(allListingPublicIDs)
+                .stream()
+                .map(listingMapper::listingToDisplayCardListingDTO)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<DisplayCardListingDTO> getByPublicIdAndLandlordPublicId(UUID listingPublicId, UUID landlordPublicId) {
+        return listingRepository.findOneByPublicIdAndLandlordPublicId(listingPublicId, landlordPublicId)
+                .map(listingMapper::listingToDisplayCardListingDTO);
+    }
+}
---
+++ airbn-clone-back/src/main/java/com/airbnb/airbn_clone_back/listing/PictureService.java
@@ -0,0 +1,41 @@
+package com.airbnb.airbn_clone_back.listing;
+
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.airbnb.airbn_clone_back.listing.application.dto.sub.PictureDTO;
+import com.airbnb.airbn_clone_back.listing.domain.Listing;
+import com.airbnb.airbn_clone_back.listing.domain.ListingPicture;
+import com.airbnb.airbn_clone_back.listing.mapper.ListingPictureMapper;
+import com.airbnb.airbn_clone_back.listing.repository.ListingPictureRepository;
+
+@Service
+public class PictureService {
+
+    private final ListingPictureRepository listingPictureRepository;
+
+    private final ListingPictureMapper listingPictureMapper;
+
+    public PictureService(ListingPictureRepository listingPictureRepository, ListingPictureMapper listingPictureMapper) {
+        this.listingPictureRepository = listingPictureRepository;
+        this.listingPictureMapper = listingPictureMapper;
+    }
+
+    public List<PictureDTO> saveAll(List<PictureDTO> pictures, Listing listing) {
+        Set<ListingPicture> listingPictures = listingPictureMapper.pictureDTOsToListingPictures(pictures);
+
+        boolean isFirst = true;
+
+        for (ListingPicture listingPicture : listingPictures) {
+            listingPicture.setCover(isFirst);
+            listingPicture.setListing(listing);
+            isFirst = false;
+        }
+
+        listingPictureRepository.saveAll(listingPictures);
+        return listingPictureMapper.listingPictureToPictureDTO(listingPictures.stream().toList());
+    }
+}
