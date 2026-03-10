4c160aa509095e23a32501a82d6218eaf611dac7
pagarciaSim <pablo.garciasimavilla@gmail.com>
2026-03-10 07:50:57 +0100

    feat: implementing home page

---
+++ airbnb-clone-front/src/app/tenant/tenant-listing.service.spec.ts
@@ -0,0 +1,17 @@
+
+import { TestBed } from '@angular/core/testing';
+import { TenantListingService } from './tenant-listing.service';
+
+
+describe('TenantListingService', () => {
+  let service: TenantListingService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TenantListingService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
